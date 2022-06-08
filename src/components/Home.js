64486588ae2d99b5c2a2e947067f48ac005a4f7a
Sasha Rodriguez Carlomagno <caballerosasha@hotmail.com>
2022-06-08 20:25:23 -0300

    homepage

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import "../App.css";
+
+const Home = () => {
+    return (
+        <div className="Home">
+                <div className="text">
+                    <h5>Te doy la bienvenida a mi web</h5>
+                    <h1>Hola, soy Alice</h1>
+                    <h2>Diseñadora Gráfica y UX/UI</h2>
+                    <h4>Soy diseñadora y me especializo en temas como packaging, retoques digitales y logotipos</h4>
+                    <div className="Buttons">
+                        <button>Conversemos</button>
+                        <button>Mis trabajos</button>
+                    </div>
+                </div>
+            
+        </div>
+    )
+}
+
+export default Home;
